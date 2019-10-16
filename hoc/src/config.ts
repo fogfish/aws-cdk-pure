0a36a78f5fbc69a475807145072877d827e45b6e
Dmitry Kolesnikov <dmkolesnikov@gmail.com>
2019-10-17 00:30:26 +0300

    fix lint comments

--- hoc/src/config.ts
+++ hoc/src/config.ts
@@ -1,16 +1,16 @@
 /**
  * Pure Functional HoC: Config/Secret Management
  */
-import { IaaC, IPure, include } from 'aws-cdk-pure'
 import * as secret from '@aws-cdk/aws-secretsmanager'
+import { IaaC, include, IPure } from 'aws-cdk-pure'
 
 const vault = include(secret.Secret.fromSecretAttributes)
 
-export function String(key: Array<string>): IPure<string> {
-  return vault(Secret(key[0])).map(x => x.secretValueFromJson(key[1]).toString())
+export function String(key: string[]): IPure<string> {
+  return vault(Config(key[0])).map(x => x.secretValueFromJson(key[1]).toString())
 }
 
-function Secret(secretArn: string): IaaC<secret.SecretAttributes> {
+function Config(secretArn: string): IaaC<secret.SecretAttributes> {
   const Secret = () => ({ secretArn })
   return Secret
 }
