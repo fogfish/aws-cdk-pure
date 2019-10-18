33c2dd13f155417f46e140668c4a9eedccd19da5
Dmitry Kolesnikov <dmkolesnikov@gmail.com>
2019-10-18 19:24:38 +0300

    Update config docs

--- hoc/src/config.ts
+++ hoc/src/config.ts
@@ -7,6 +7,12 @@ import { IaaC, include, IPure } from 'aws-cdk-pure'
 const defaultBucket = process.env.AWS_IAAC_CONFIG || 'undefined'
 const vault = include(secret.Secret.fromSecretAttributes)
 
+/**
+ * returns a configuration as string value for given key as it is stored by AWS Secret Manager 
+ *   
+ * @param key name of the key
+ * @param bucket AWS Secret Manager bucket, the value of AWS_IAAC_CONFIG env var is used as default bucket,
+ */
 export function String(key: string, bucket: string = defaultBucket): IPure<string> {
   return vault(Config(bucket)).map(x => x.secretValueFromJson(key).toString())
 }
