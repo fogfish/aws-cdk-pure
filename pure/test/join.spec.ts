bfdf7918e8eabba6f3db52dcda575ca5bcacdaad
Dmitry Kolesnikov <dmkolesnikov@gmail.com>
2019-10-19 15:30:08 +0300

    Increase test coverage of aws-cdk-pure lib

--- pure/test/join.spec.ts
+++ pure/test/join.spec.ts
@@ -1,32 +1,32 @@
 import { expect } from 'chai'
-import { root, join } from '../src/index'
+import * as pure from '../src/index'
 import * as cdk from '@aws-cdk/core'
 
-function ResourceA(node: cdk.Construct): cdk.Construct {
-  return new cdk.CfnResource(node, 'ResourceA', { type: 'TypeA' })
+function MyA(node: cdk.Construct): cdk.Construct {
+  return new cdk.CfnResource(node, 'MyA', { type: 'A' })
 }
 
-function ResourceB(node: cdk.Construct): cdk.Construct {
-  return new cdk.CfnResource(node, 'ResourceB', { type: 'TypeB' })
+function MyB(node: cdk.Construct): cdk.Construct {
+  return new cdk.CfnResource(node, 'MyB', { type: 'B' })
 }
 
 function Stack(scope: cdk.Construct): cdk.Construct {
-  join(scope, ResourceA)
-  join(scope, ResourceB)
+  pure.join(scope, MyA)
+  pure.join(scope, MyB)
   return scope
 }
 
 it('attach components to stack',
   () => {
     const app = new cdk.App()
-    root(app, Stack, 'IaaC')
+    pure.root(app, Stack, 'IaaC')
     const response = app.synth()
     const stack = response.getStack('IaaC')
     expect(stack.template).deep.equal(
       {
         Resources: { 
-          ResourceA: { Type: 'TypeA' },
-          ResourceB: { Type: 'TypeB' },
+          MyA: { Type: 'A' },
+          MyB: { Type: 'B' },
         }
       }
     )
--- pure/test/map.spec.ts
+++ pure/test/map.spec.ts
@@ -1,40 +1,40 @@
 import { expect } from 'chai'
-import { IaaC, root, join, iaac } from '../src/index'
+import * as pure from '../src/index'
 import * as cdk from '@aws-cdk/core'
 
-const cf = iaac(cdk.CfnResource)
+const cf = pure.iaac(cdk.CfnResource)
 
-function ResourceA(): cdk.CfnResourceProps {
-  return { type: 'TypeA' }
+function MyA(): cdk.CfnResourceProps {
+  return { type: 'A' }
 }
 
-function ResourceB(): cdk.CfnResourceProps {
-  return { type: 'TypeB' }
+function MyB(): cdk.CfnResourceProps {
+  return { type: 'B' }
 }
 
-function ResourceC(): IaaC<cdk.CfnResource> {
-  return cf(ResourceA)
+function MyC(): pure.IaaC<cdk.CfnResource> {
+  return cf(MyA)
     .flatMap(a => 
-      cf(ResourceB).effect(b => b.addOverride('Other', a.logicalId))
+      cf(MyB).effect(b => b.addOverride('Other', a.logicalId))
     )
 }
 
 function Stack(scope: cdk.Construct): cdk.Construct {
-  join(scope, ResourceC)
+  pure.join(scope, MyC)
   return scope
 }
 
 it('apply flatmap to pure functional component',
   () => {
     const app = new cdk.App()
-    root(app, Stack, 'IaaC')
+    pure.root(app, Stack, 'IaaC')
     const response = app.synth()
     const stack = response.getStack('IaaC')
     expect(stack.template).deep.equal(
       {
         Resources: { 
-          ResourceA: { Type: 'TypeA' },
-          ResourceB: { Type: 'TypeB', Other: 'ResourceA' },
+          MyA: { Type: 'A' },
+          MyB: { Type: 'B', Other: 'MyA' },
         }
       }
     )
