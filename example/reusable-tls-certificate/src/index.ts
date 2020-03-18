f46d70e506198cf7833eee9951660511252a3b61
Dmitry Kolesnikov <dmkolesnikov@gmail.com>
2020-03-18 21:55:13 +0200

    add example of reusable tls certifiate

---
+++ example/reusable-tls-certificate/src/index.ts
@@ -0,0 +1,47 @@
+import * as cdk from '@aws-cdk/core'
+import * as pure from 'aws-cdk-pure'
+import * as hoc from 'aws-cdk-pure-hoc'
+
+//
+// Global configuration, the tls certificates requires few parameters
+//  * root domain of the site (e.g. example.com). It must correspond to 
+//    existing entity in AWS Hosted Zone
+//  * subdomain of web-site or api (e.g. www | api)
+//
+const app = new cdk.App()
+const stack = {
+  env: {
+    account: process.env.CDK_DEFAULT_ACCOUNT,
+    region: process.env.CDK_DEFAULT_REGION,
+  }
+}
+const subdomain = String(app.node.tryGetContext('subdomain'))
+const domain = String(app.node.tryGetContext('domain'))
+
+//
+// Common stack creates a certificate and exports its.
+// The stack is deployed only once during life-cycle of application.
+//
+const tlsCertificate = pure.join(
+  new cdk.Stack(app, 'common', stack),
+  hoc.common.HostedZone(domain).flatMap(
+    zone => hoc.common.Certificate(`*.${domain}`, zone)
+  ),
+).certificateArn
+
+//
+// Application stack deploy application resource, which depends on
+// existing tlsCertificate. The application stack can be destroyed/deployed
+// unlimited amount of time. It would not cause re-create of certificate. 
+pure.join(
+  new cdk.Stack(app, 'application', stack),
+  hoc.staticweb.Gateway({
+    domain,
+    subdomain,
+    tlsCertificate,
+    sites: [{origin: '', site: 'api'}],
+  }),
+)
+
+
+
--- hoc/src/index.ts
+++ hoc/src/index.ts
@@ -5,6 +5,7 @@
 // of the MIT license.  See the LICENSE file for details.
 // https://github.com/fogfish/aws-cdk-pure
 //
+export import common = require('./hoc')
 export import config = require('./config')
 export import gateway = require('./gateway')
 export import staticweb = require('./staticweb')
