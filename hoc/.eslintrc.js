4e8f0b3f39ba4b30277ddd11523d45cd06bc4d3b
Dmitry Kolesnikov <dmkolesnikov@gmail.com>
2021-05-11 22:41:21 +0300

    drop tslint

---
+++ hoc/.eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+  ],
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  "rules": {
+    "semi": "off",
+    "@typescript-eslint/semi": ["off"],
+  }
+}
